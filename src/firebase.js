3666d4dcbf9a5d8a5079194c70c95eabae78c379
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-03-07 18:04:11 +0000

    Firebase for auth and .env setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
